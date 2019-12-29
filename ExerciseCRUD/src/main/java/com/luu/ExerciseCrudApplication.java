4567a2be8698045ceeb87eec06c15725288640ca
DucLuu <luudo0309@gmail.com>
2019-12-29 23:39:01 +0700

    Tao project

---
+++ ExerciseCRUD/src/main/java/com/luu/ExerciseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.luu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseCrudApplication.class, args);
+	}
+
+}
---
+++ ExerciseCRUD/src/test/java/com/luu/ExerciseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
